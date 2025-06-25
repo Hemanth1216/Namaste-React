a54f9dbd08cb82381fee74f164f24df75bf0f4a9
Hemanth1216 <narrahemanth83@gmail.com>
2025-06-25 19:31:50 +0530

    episode-3 code

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
- const heading = React.createElement("h1", {id: "heading"}, "Hi, from React") // give me a React object to create an element.
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading) // converts react object into DOM element
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hi, from React") // give me a React object to create an element.
+const heading = <h1 id="heading">Hi, I'm React</h1>
+
+const HeaderComponent = () => {
+    return <h1 className="heading">I'm Header component</h1>
+}
+
+const HeaderComponentalt = () => <h1 className="heading">I'm alt Header component</h1>
+
+const NestedHeaderComponent = () => (
+    <div>
+        {heading}
+        <HeaderComponent />
+        <HeaderComponentalt />
+        <h1 className="heading">I'm Nested Header component</h1>
+    </div>
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<NestedHeaderComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- HMR - Hot Module Replacement / Hot Reload
+- Local server
+- File Watch Algorithm - written in c++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
+- Diagnostic
+- Error handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
